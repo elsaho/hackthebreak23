67441424245ac4315b4a42e1659e1aa9f8d4f0e9
elsaho <elsa.ho98@gmail.com>
2023-03-11 13:02:04 -0800

    added a navbar

---
+++ src/components/AlertComponent.js
@@ -0,0 +1,25 @@
+import React from 'react';
+import Alert from 'react-bootstrap/Alert';
+
+function AlertComponent() {
+  return (
+    <>
+      {[
+        'primary',
+        'secondary',
+        'success',
+        'danger',
+        'warning',
+        'info',
+        'light',
+        'dark',
+      ].map((variant) => (
+        <Alert key={variant} variant={variant}>
+          This is a {variant} alert—check it out!
+        </Alert>
+      ))}
+    </>
+  );
+}
+
+export default AlertComponent;

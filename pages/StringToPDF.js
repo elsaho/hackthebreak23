821872aa3a8f30d1121f29b6e0b3f10fb6c26168
elsaho <elsa.ho98@gmail.com>
2023-03-11 18:36:37 -0800

    can add line break in pdf

--- pages/StringToPDF.js
+++ pages/StringToPDF.js
@@ -20,29 +20,35 @@ class StringToPDF extends React.Component {
     const { width, height } = page.getSize();
     const fontSize = 12;
     const font = await pdfDoc.embedFont(StandardFonts.Helvetica);
-    const stringToConvert = this.state.stringToConvert;
-    const textWidth = font.widthOfTextAtSize(stringToConvert, fontSize);
-    const textHeight = font.heightAtSize(fontSize);
-    page.drawText(stringToConvert, {
-      x: (width - textWidth) / 2,
-      y: (height - textHeight) / 2,
-      size: fontSize,
-      font: font,
-    });
+    const stringToConvert = this.state.stringToConvert.replace(/\n/g, '\u00A0');
+    const maxWidth = 500; // set the maximum width for the text
+    const lines = font.layout(stringToConvert, { fontSize, maxWidth });
+    const textHeight = lines.reduce((height, line) => height + line.height, 0);
+    let y = (height - textHeight) / 2;
+    for (const line of lines) {
+      page.drawText(line.text, {
+        x: (width - line.width) / 2,
+        y,
+        size: fontSize,
+        font: font,
+      });
+      y -= line.height;
+    }
     const pdfBytes = await pdfDoc.save();
     const blob = new Blob([pdfBytes], { type: 'application/pdf' });
     saveAs(blob, 'output.pdf');
   }
+  
 
-  handleDownloadWord = () => {
-    const doc = new Document();
-    doc.addSection({
-      children: [new Paragraph(this.state.stringToConvert)],
-    });
-    Packer.toBlob(doc).then((blob) => {
-      saveAs(blob, 'output.docx');
-    });
-  }
+  // handleDownloadWord = () => {
+  //   const doc = new Document();
+  //   doc.addSection({
+  //     children: [new Paragraph(this.state.stringToConvert)],
+  //   });
+  //   Packer.toBlob(doc).then((blob) => {
+  //     saveAs(blob, 'output.docx');
+  //   });
+  // }
 
   render() {
     return (

d0119cd9e217534ea721845f959faaf3d8ac923a
CyrusVillaruz <cyrusvillaruz1@gmail.com>
2023-03-11 22:45:18 -0800

    SPIN MK.II

--- pages/index.js
+++ pages/index.js
@@ -1,8 +1,7 @@
 import Head from "next/head";
 import { useState } from "react";
 import styles from "./index.module.css";
-import StringToPDF from './StringToPDF';
-
+import StringToPDF from "./StringToPDF";
 
 export default function Home() {
   const [jobTitleInput, setJobTitleInput] = useState("");
@@ -16,7 +15,7 @@ export default function Home() {
 
   async function onSubmit(event) {
     event.preventDefault();
-    setIsButtonClicked(true); // Set the button clicked state to true
+    setIsButtonClicked(true);
     if (
       !jobTitleInput.trim() ||
       !skillInput.trim() ||
@@ -43,30 +42,32 @@ export default function Home() {
           name: nameInput,
         }),
       });
-  
+
       const data = await response.json();
       if (response.status !== 200) {
-        throw data.error || new Error(`Request failed with status ${response.status}`);
+        throw (
+          data.error ||
+          new Error(`Request failed with status ${response.status}`)
+        );
       }
       function removeNewlineBeforeWords(s) {
         // Define pattern to match newline character before words
         const pattern = /^[\n]+([A-Za-z])/;
-      
+
         // Replace matched pattern with the first letter of the word
-        return s.replace(pattern, '$1');
+        return s.replace(pattern, "$1");
       }
       function removeNewlineAfterWords(s) {
         // Define pattern to match newline character after words
         const pattern = /([A-Za-z])[ \t]*[\n]+/g;
-      
+
         // Replace matched pattern with the first letter of the word
-        return s.replace(pattern, '$1 ');
+        return s.replace(pattern, "$1 ");
       }
-      
-  
-      setResult(removeNewlineAfterWords(
-        removeNewlineBeforeWords(
-          data.result.trim())));
+
+      setResult(
+        removeNewlineAfterWords(removeNewlineBeforeWords(data.result.trim()))
+      );
       setJobTitleInput("");
       setSkillInput("");
       setPositionInput("");
@@ -77,79 +78,114 @@ export default function Home() {
       // Consider implementing your own error handling logic here
       console.error(error);
       alert(error.message);
+    } finally {
+      setIsButtonClicked(false);
     }
   }
 
-// Randomizer: Generate random job title
-function randomJobTitle() {
-  const jobTitles = ["Software Engineer", "Piano Teacher", "Guest Experience Expert", "Baker", "Pro Gamer",
-"Swim Instructor", "Painter", "Comedian"];
-  const randomIndex = Math.floor(Math.random() * jobTitles.length);
-  return jobTitles[randomIndex];
-}
+  // Randomizer: Generate random job title
+  function randomJobTitle() {
+    const jobTitles = [
+      "Software Engineer",
+      "Piano Teacher",
+      "Guest Experience Expert",
+      "Baker",
+      "Pro Gamer",
+      "Swim Instructor",
+      "Painter",
+      "Comedian",
+    ];
+    const randomIndex = Math.floor(Math.random() * jobTitles.length);
+    return jobTitles[randomIndex];
+  }
 
-function randomSkill() {
-  const skills = ["Multi-tasking", "Being organized", "Juggling", "Java", "C", "Python", "Running really fast",
-"Eating a lot of food"];
-  const randomIndex = Math.floor(Math.random() * skills.length);
-  return skills[randomIndex];
-}
+  function randomSkill() {
+    const skills = [
+      "Multi-tasking",
+      "Being organized",
+      "Juggling",
+      "Java",
+      "C",
+      "Python",
+      "Running really fast",
+      "Eating a lot of food",
+    ];
+    const randomIndex = Math.floor(Math.random() * skills.length);
+    return skills[randomIndex];
+  }
 
-function randomPosition() {
-  const position = ["Student", "Tutor", "Freelancer"];
-  const randomIndex = Math.floor(Math.random() * position.length);
-  return position[randomIndex];
-}
+  function randomPosition() {
+    const position = ["Student", "Tutor", "Freelancer"];
+    const randomIndex = Math.floor(Math.random() * position.length);
+    return position[randomIndex];
+  }
 
-function randomDuration() {
-  const duration = ["1 year", "3 months", "5 years", "6 months"];
-  const randomIndex = Math.floor(Math.random() * duration.length);
-  return duration[randomIndex];
-}
+  function randomDuration() {
+    const duration = ["1 year", "3 months", "5 years", "6 months"];
+    const randomIndex = Math.floor(Math.random() * duration.length);
+    return duration[randomIndex];
+  }
 
-function randomDescription() {
-  const description = ["Team leader", "Tried not to break anything", "Kept operations running", "Linked list master"];
-  const randomIndex = Math.floor(Math.random() * description.length);
-  return description[randomIndex];
-}
+  function randomDescription() {
+    const description = [
+      "Team leader",
+      "Tried not to break anything",
+      "Kept operations running",
+      "Linked list master",
+    ];
+    const randomIndex = Math.floor(Math.random() * description.length);
+    return description[randomIndex];
+  }
 
-function randomName() {
-  const name = ["Elsa", "Mai", "Cyrus", "Nico", "Tomek", "Justin", "Trevor", "Troy", "Kate", "Haurence", "Turtino"];
-  const randomIndex = Math.floor(Math.random() * name.length);
-  return name[randomIndex];
-}
+  function randomName() {
+    const name = [
+      "Elsa",
+      "Mai",
+      "Cyrus",
+      "Nico",
+      "Tomek",
+      "Justin",
+      "Trevor",
+      "Troy",
+      "Kate",
+      "Haurence",
+      "Turtino",
+    ];
+    const randomIndex = Math.floor(Math.random() * name.length);
+    return name[randomIndex];
+  }
 
-function onSurpriseClick() {
-  setJobTitleInput(randomJobTitle());
-  setSkillInput(randomSkill());
-  setPositionInput(randomPosition());
-  setDurationInput(randomDuration());
-  setDescriptionInput(randomDescription());
-  setNameInput(randomName());
-}
+  function onSurpriseClick() {
+    setJobTitleInput(randomJobTitle());
+    setSkillInput(randomSkill());
+    setPositionInput(randomPosition());
+    setDurationInput(randomDuration());
+    setDescriptionInput(randomDescription());
+    setNameInput(randomName());
+  }
 
+  function Icon(props) {
+    const className = isButtonClicked
+      ? `index_icon__CgRrC icon`
+      : props.className;
+    return (
+      <img src={props.src} id="cowboy" className={className} alt={props.alt} />
+    );
+  }
 
-
-
-function Icon(props) {
-  const className = isButtonClicked ? `index_icon__CgRrC icon` : props.className;
-  return <img src={props.src} id="cowboy" className={className} alt={props.alt} />;
-}
-
-
-return (
-  // <div style={{backgroundColor: '#FFFFF0'}}>
-  <div>
-    <Head>
-      <title>Cover Letter Generator</title>
-      <link rel="icon" href="/favicon.png" />
-    </Head>
-    <main className={styles.main}>
-      <Icon
-        src="https://cdn.discordapp.com/attachments/1030235742963769366/1084359941583814696/favicon-32x32.png"
-        className="cowboy"
-        alt="cowboy emoji"
-      />
+  return (
+    // <div style={{backgroundColor: '#FFFFF0'}}>
+    <div>
+      <Head>
+        <title>Cover Letter Generator</title>
+        <link rel="icon" href="/favicon.png" />
+      </Head>
+      <main className={styles.main}>
+        <Icon
+          src="https://cdn.discordapp.com/attachments/1030235742963769366/1084359941583814696/favicon-32x32.png"
+          className="cowboy"
+          alt="cowboy emoji"
+        />
         <h3>Cover Letter Generator</h3>
         <form onSubmit={onSubmit}>
           <input
@@ -195,10 +231,9 @@ return (
             onChange={(e) => setNameInput(e.target.value)}
           />
           <input type="button" value="Surprise me!" onClick={onSurpriseClick} />
-          <input type="submit" value="Generate cover letter"/>
+          <input type="submit" value="Generate cover letter" />
         </form>
-        <StringToPDF stringToConvert= {result}/>
-        
+        <StringToPDF stringToConvert={result} />
       </main>
       {/* <StringToPDF stringToConvert= {result}/> */}
     </div>
